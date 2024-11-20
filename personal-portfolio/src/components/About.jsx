c83b8a379c03744f96f542a164ecfe416322292c
Henriette <henuwz@gmail.com>
2024-11-20 21:38:30 +0200

    About Me heading and aboutme Image

--- personal-portfolio/src/components/About.jsx
+++ personal-portfolio/src/components/About.jsx
@@ -1,9 +1,26 @@
-'use client'
+"use client";
+
+import Heading from "./sub/Heading";
+import Achievements from "./sub/Achievements"
+import Image from "next/image";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="min-h-screen px-96 flex flex-col items-center justify-center">
+      <Heading text={'About Me'} />
+      <div>
+        <Image src={'/aboutme.png'} alt="About Me picture" width={400} height={400} />
+        <div>
+            <span>Arrow Left</span>
+            <p>About text</p>
+            <a href="/ResumeH.pdf" download="">
+            <span>Download Resume</span>
+            <span>Download Icon</span>
+            </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
---
+++ personal-portfolio/src/components/sub/Achievements.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Achievements = () => {
+  return (
+    <div>
+        <span>Icon</span>
+        <h1>
+            <span>200</span>
+            <span>Github Repos</span>
+        </h1>
+    </div>
+  )
+}
+
+export default Achievements
