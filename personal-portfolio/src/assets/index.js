20ea03f7a3afc5a47f558e4e80f5af9277c610ed
Henriette <henuwz@gmail.com>
2024-11-20 22:26:06 +0200

    About text

--- personal-portfolio/src/assets/index.js
+++ personal-portfolio/src/assets/index.js
@@ -1,38 +1,49 @@
-import InstagramLineIcon from 'remixicon-react/InstagramLineIcon'
-import DribbleLineIcon from 'remixicon-react/DribbbleLineIcon'
-import GithubLineIcon from 'remixicon-react/GithubLineIcon'
+import InstagramLineIcon from "remixicon-react/InstagramLineIcon";
+import DribbleLineIcon from "remixicon-react/DribbbleLineIcon";
+import GithubLineIcon from "remixicon-react/GithubLineIcon";
 
 export const heroIcons = [
-    <InstagramLineIcon />,
-    <DribbleLineIcon />,
-    <GithubLineIcon />,
-]
+  <InstagramLineIcon />,
+  <DribbleLineIcon />,
+  <GithubLineIcon />,
+];
 
 //About Me Icons
-import GithubFillIcon from 'remixicon-react/GithubFillIcon'
-import Projector2LineIcon from 'remixicon-react/Projector2LineIcon'
-import GroupLineIcon from 'remixicon-react/Group2LineIcon'
-import AwardFillIcon from 'remixicon-react/AwardFillIcon'
+import GithubFillIcon from "remixicon-react/GithubFillIcon";
+import Projector2LineIcon from "remixicon-react/Projector2LineIcon";
+import GroupLineIcon from "remixicon-react/Group2LineIcon";
+import AwardFillIcon from "remixicon-react/AwardFillIcon";
 
 export const aboutData = [
-    {
-        title: 'Github Repos',
-        amount: 146,
-        icon: <GithubFillIcon />
-    },
-    {
-        title: 'Successful Projects',
-        amount: '100',
-        icon: <Projector2LineIcon />
-    },
-    {
-        title: 'Satisfied clients',
-        amount: '80',
-        icon: <GroupLineIcon />
-    },
-    {
-        title: 'Certificates',
-        amount: 50,
-        icon: <AwardFillIcon />
-    },
-]
+  {
+    title: "Github Repos",
+    amount: 146,
+    icon: <GithubFillIcon />,
+  },
+  {
+    title: "Successful Projects",
+    amount: "100",
+    icon: <Projector2LineIcon />,
+  },
+  {
+    title: "Satisfied clients",
+    amount: "80",
+    icon: <GroupLineIcon />,
+  },
+  {
+    title: "Certificates",
+    amount: 50,
+    icon: <AwardFillIcon />,
+  },
+];
+
+import DownloadLineIcon from "remixicon-react";
+import ArrowLeftSFillIcon from "remixicon-react";
+
+export const downloadIcon = <DownloadLineIcon />;
+export const arrowLeftIcon = <ArrowLeftSFillIcon />;
+
+export const aboutText =
+  " Hi, I'm Henriette, a Frontend Developer & Designer. I specialize in crafting visually stunning and user-friendly websites. Using Figma, I design clean and modern interfaces that prioritize user experience. On the development side, I bring these designs to life with React.js, Next.js, and Tailwind CSS, ensuring fast, responsive, and interactive web applications. Let's create something extraordinary together!"
+  //End of About me
+  
--- personal-portfolio/src/components/About.jsx
+++ personal-portfolio/src/components/About.jsx
@@ -3,13 +3,14 @@
 import Heading from "./sub/Heading";
 import Achievements from "./sub/Achievements"
 import Image from "next/image";
+import { aboutData, aboutText, downloadIcon, arrowLeftIcon } from "@/assets";
 
 const About = () => {
   return (
     <div className="min-h-screen px-96 flex flex-col items-center justify-center">
       <Heading text={'About Me'} />
-      <div>
-        <Image src={'/aboutme.png'} alt="About Me picture" width={400} height={400} />
+      <div className="w-full flex items-center justify-between md:justify-center">
+        <Image src={'/aboutme.png'} alt="About Me picture" width={400} height={400} className="w-[300px] lg:w-[200px] md:hidden" />
         <div>
             <span>Arrow Left</span>
             <p>About text</p>
