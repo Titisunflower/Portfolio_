1051b7df19ee330d24395ed038cc52dd289e52d8
Henriette <henuwz@gmail.com>
2024-11-20 20:50:12 +0200

    Done with Hero section starting About me section

--- personal-portfolio/src/components/Hero.jsx
+++ personal-portfolio/src/components/Hero.jsx
@@ -1,40 +1,61 @@
 "use client";
 import Image from "next/image";
 import { heroIcons } from "@/assets";
-import { useMotionValue, useTransform } from "framer-motion";
-import { useState } from "react"
+import { useMotionValue, useTransform, motion, useSpring } from "framer-motion";
+import { useState } from "react";
 
 const Hero = () => {
-    const [windowOffset, setWindowOffset] = useState({ innerWidth:0, innerHeight: 0 })
-    const [onMouseMove, setMouseMove] = useState(false)
-    const x = useMotionValue(0)
-    const y = useMotionValue(0)
+  const [windowOffset, setWindowOffset] = useState({
+    innerWidth: 0,
+    innerHeight: 0,
+  });
+  const [onMouseMove, setMouseMove] = useState(false);
+  const [buttonHover, setButtonHover] = useState(false);
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
 
-    const handleMouseMove = (e) => {
-      const {clientX, clientY} = e
-      x.set(clientX)
-      y.set(clientY)
+  const handleMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    x.set(clientX);
+    y.set(clientY);
 
-      console.log(clientX, clientY, x, y)
-    }
+    console.log(clientX, clientY, x, y);
+  };
 
-    const handleMouseEnter = () => {
-      setWindowOffset({ innerWidth: window.innerWidth, innerHeight: window.innerHeight })
-      setMouseMove(true)
+  const handleMouseEnter = () => {
+    setWindowOffset({
+      innerWidth: window.innerWidth,
+      innerHeight: window.innerHeight,
+    });
+    setMouseMove(true);
 
-      console.log(innerWidth, innerHeight)
-    }
+    console.log(innerWidth, innerHeight);
+  };
 
-    const { innerWidth, innerHeight } = windowOffset
+  const { innerWidth, innerHeight } = windowOffset;
 
-    const rotatY = useTransform(x, [0, innerWidth], [-30, 30])
-    const rotatX = useTransform(y, [0, innerHeight], [10, -50])
+  const xSpring = useSpring(x, { stiffness: 100, damping: 10 });
+  const ySpring = useSpring(y, { stiffness: 100, damping: 10 });
+
+  const rotateY = useTransform(xSpring, [0, innerWidth], [-30, 30]);
+  const rotateX = useTransform(ySpring, [0, innerHeight], [10, -50]);
 
   return (
-    <div className="h-screen grid place-items-center" onMouseMove={handleMouseMove} onMouseEnter= {handleMouseEnter}>
+    <div
+      className="h-screen grid place-items-center"
+      onMouseMove={handleMouseMove}
+      onMouseEnter={handleMouseEnter}
+    >
       <div>
         <div className="flex flex-col items-center justify-center gap-y-3 font-light capitalize">
-          <div className="flex items-center justify-center">
+          <motion.div
+            className="flex items-center justify-center"
+            style={{
+              rotateX: onMouseMove ? rotateX : 0,
+              rotateY: onMouseMove ? rotateY : 0,
+              transition: "0.1s",
+            }}
+          >
             <Image
               src={"/person.png"}
               alt="Person Image"
@@ -43,19 +64,45 @@ const Hero = () => {
               priority={true}
               className="h-auto w-[150px]"
             />
-            <span className="absolute text-3xl font-semibold text-white">Hi</span>
-          </div>
-          <h1 className="text-center text-3xl font-bold tracking-wider text-gray-500">My Name is Henriette &</h1>
-          <p className="text-lg tracking-wider text-gray-700">I Like Design 🥰</p>
+            <motion.span
+              className="absolute text-3xl font-semibold text-white"
+              initial={{ scale: 0 }}
+              animate={{
+                opacity: buttonHover ? 0 : 1,
+                scale: buttonHover ? 2 : 0,
+                y: buttonHover ? -40 : 0,
+              }}
+              transition={{ opacity: { delay: 0.4 } }}
+            >
+              Hi
+            </motion.span>
+          </motion.div>
+          <h1 className="text-center text-3xl font-bold tracking-wider text-gray-500 sm:text-2xl">
+            My Name is Henriette &
+          </h1>
+          <p className="text-lg tracking-wider text-gray-700">
+            I Like Design 🥰
+          </p>
         </div>
-        <div className="mt-8 flex justify-center gap-x-10 text-3xl text-customPink">
-            {heroIcons.map((icon, i) => (
-            <a href="#" key={i} className="rounded-lg hover:bg-pink-300 hover:text-black transition-colors">
-                {icon}
+        <div className="mt-8 flex justify-center gap-x-10 text-3xl text-customPink sm:text-2xl">
+          {heroIcons.map((icon, i) => (
+            <a
+              href="#"
+              key={i}
+              className="rounded-lg hover:bg-pink-300 hover:text-black transition-colors"
+            >
+              {icon}
             </a>
-            ))}
+          ))}
         </div>
-        <a href="" className="mx-auto mt-7 block w-max rounded-lg bg-customPurple px-3 py-1 font-light capitalize tracking-wider hover:bg-purple-400 transition-colors">Let's Connect</a>
+        <a
+          href=""
+          className="mx-auto mt-7 block w-max rounded-lg bg-customPurple px-3 py-1 font-light capitalize tracking-wider hover:bg-purple-400 transition-colors"
+          onMouseEnter={() => setMouseMove(true)}
+          onMouseLeave={() => setButtonHover(false)}
+        >
+          Let's Connect
+        </a>
       </div>
     </div>
   );
--- personal-portfolio/tailwind.config.js
+++ personal-portfolio/tailwind.config.js
@@ -15,6 +15,12 @@ module.exports = {
         purpleColor: "#7447E4",
       },
     },
+      screens: {
+        xl: {max: '1400px'},
+        lg: {max: '1150px'},
+        md: {max: '767px'},
+        sm: {max: '639px'},
+      }
   },
   plugins: [],
 };
