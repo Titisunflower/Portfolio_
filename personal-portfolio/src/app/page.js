01f0861647d071a1bc4b558330e9871ead1f4d17
Henriette <henuwz@gmail.com>
2024-11-22 19:41:44 +0200

    Started Experience

--- personal-portfolio/src/app/page.js
+++ personal-portfolio/src/app/page.js
@@ -2,12 +2,14 @@
 
 import Hero from "@/components/Hero";
 import About from "@/components/About";
+import Experience from "@/components/Experience";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <About />
+      <Experience />
     </div>
   );
 }
